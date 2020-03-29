030e56435d1bff1c18fce67b558811007f23c2b7
KOUAM Ledoux <kouamdoux@gmail.com>
2020-03-29 23:02:13 +0200

    Add unit tests for memmove()

--- src/tests/mem/memmove.c
+++ src/tests/mem/memmove.c
@@ -2,63 +2,94 @@
 
 EMBOX_TEST_SUITE("Test suite for memmove()") ;
 
-TEST_CASE("memmove with destination with zero size")
+TEST_CASE("Verify the address return by memmove is equal to the destination")
+{
+    const char *src ;
+    char *dest , *dest_tmp  ;
+
+    dest = (char*)malloc(sizeof(char)* strlen(src)) ;
+
+    memmove((void*)dest , (void*)src , (size_t)strlen(src)) ;
+    
+    *dest_tmp = *dest ;
+
+    test_assert_not_null(dest); //It should be not null
+    test_assert_null(src) ;
+    test_assert_zero(strncmp(dest , dest_tmp)) ;
+}
+
+TEST_CASE("move full data")
 {
     const char src[] = "string" ;
-    char dst[sizeof(src)+1] ;
+    char *dest , *dest_tmp;
 
-    dst[0] = '\0' ;
+    dest = (char*)malloc(sizeof(char) * strlen(src)) ;
 
-    test_assert_zero(strlen(memmove(dst , src , 0))) ;
+    memmove(dest , src , strlen(src)) ;
+    
+    *dest_tmp = *dest ;
+
+    test_assert_not_null(dest);
+    test_assert_null(src) ;
+    test_assert_zero(strncmp(dest , dest_tmp)) ;
 }
 
-TEST_CASE("move string with memmove")
+TEST_CASE("check for NULL argument")
 {
-    const char src[] = "string" ;
-    char dst[sizeof(src) + 1] ;
+    const char src[] = "\0" ;
+    char *dest , *dest_tmp ;
+    
+    dest = (char*)malloc(sizeof(char) * strlen(src)) ;
 
-    memset(dst , 0x55 , sizeof(dst));
+    memmove(dest , src  , strlen(src)) ;
+    
+    *dest_tmp = *dest ;
 
-    test_assert_equal(strlen(src),\
-		strlen(memmove(dst, src, sizeof(dst))));
-	test_assert_zero(strcmp(dst, src));
+    test_assert_not_null(dest);
+    test_assert_null(src) ;
+    test_assert_zero(strncmp(dest , dest_tmp)) ;
 }
 
-TEST_CASE("move integer array with memmove")
+TEST_CASE("move array")
 {
-    const int src[] = {1,2,3,4,5} ;
-    int dst[sizeof(src) / sizeof(int)];
+    const char src[] = {'a', 't', 'h', '-', '9', '0'};
+    char *dest ;
 
-    memset(dst , 0x55 , sizeof(dst)) ;
-    memmove(dst , src , sizeof(dst)) ;
+    dest = (char*)malloc(sizeof(char) * strlen(src)) ;
 
-    test_assert_zero(memmove(src , dst , (sizeof(dst)/sizeof(int)))) ;
+    memmove(dest , src  , strlen(src)) ;
 
+    test_assert_not_null(dest);
+    test_assert_null(src) ;
+    test_assert_zero(strncmp(dest , dest_tmp)) ;
 }
 
-TEST_CASE("move char array with null in middle")
+TEST_CASE("move array with null in the middle")
 {
-    const char src[] = {'a' , 't' , 'h' , '\0' , '-' , '9' , '0'} ;
-    char dst[sizeof(src)] ;
+    const char src[] = {'a', 't', 'h', '\0' ,'-', '9', '0'};
+    char *dest ;
 
-    memset(dst , 0x55 , sizeof(dst)) ;
-    memcpy(dst , src , sizeof(dst)) ;
-    test_assert_zero(memmove(src , dst , sizeof(dst))) ;
+    dest = (char*)malloc(sizeof(char) * strlen(src)) ;
+
+    memmove(dest , src  , strlen(src)) ;
+
+    test_assert_not_null(dest);
+    test_assert_null(src) ;
+    test_assert_zero(strncmp(dest , dest_tmp)) ;
 }
 
-TEST_CASE("Move char array whitout overflow")
+TEST_CASE("If memory is not allocated enough")
 {
-    const char src[50] ;
-    char dst[150] ;
+    const char src[]= "string" ;
+    char dest[2] , *dest_tmp ;
+    
+    dest = (char*)malloc(sizeof(char) * strlen(src)) ;
 
-    memset(dst , 0 , sizeof(dst)) ;
-    memset(src , 0x55 , sizeof(src)) ;
-    memcpy(&dst[50] , src , sizeof(src)) ;
+    memmove(dest , src , strlen(src)) ;
 
-    test_assert_zero(memmove(src , &dst[50] , sizeof(src))) ;
+    *dest_tmp = *dest ;
 
-    /* Check if memcpy has not modified beyond the boundary */
-	test_assert_zero(dst[49]);
-	test_assert_zero(dst[100]);
-
+    test_assert_not_null(dest);
+    test_assert_null(src) ;
+    test_assert_zero(strncmp(dest , dest_tmp)) ;
 }
